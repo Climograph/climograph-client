fe05cee90acbad0de080d18839a4b93a21dcd2ae
Andrew Kononenko <wastardy.k@gmail.com>
2026-03-12 22:31:42 +0100

    feat(routes): register home page route

--- src/constants/route.constant.ts
+++ src/constants/route.constant.ts
@@ -1,3 +1,3 @@
-export const Route = {
+export const ROUTES = {
   HOME: "/",
 } as const;
--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,3 +1,11 @@
+import { ROUTES } from "@/constants";
+import { HomePage } from "@/pages";
+import { createElement } from "react";
 import { createBrowserRouter } from "react-router-dom";
 
-export const router = createBrowserRouter([]);
+export const router = createBrowserRouter([
+  {
+    path: ROUTES.HOME,
+    element: createElement(HomePage),
+  },
+]);
