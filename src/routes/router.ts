f635c2c2177aa11ac3b6c4c35e1102dc54cff804
Andrew Kononenko <wastardy.k@gmail.com>
2026-04-06 19:32:06 +0200

    feat(routes): update router to use Layout component for HomePage

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,3 +1,4 @@
+import { Layout } from "@/components/Layout";
 import { ROUTES } from "@/constants";
 import { HomePage } from "@/pages";
 import { createElement } from "react";
@@ -6,6 +7,12 @@ import { createBrowserRouter } from "react-router-dom";
 export const router = createBrowserRouter([
   {
     path: ROUTES.HOME,
-    element: createElement(HomePage),
+    element: createElement(Layout),
+    children: [
+      {
+        index: true,
+        element: createElement(HomePage),
+      },
+    ],
   },
 ]);
