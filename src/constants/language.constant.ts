efa79d84fb52f980c3eb7ac5ef3ea6b71d1ea893
Andrew Kononenko <wastardy.k@gmail.com>
2026-04-07 22:40:45 +0200

    feat(constants): add default language and supported languages codes constants

--- src/constants/language.constant.ts
+++ src/constants/language.constant.ts
@@ -3,3 +3,7 @@ export const LANGUAGES = [
   { code: "es", label: "ES 🇪🇸" },
   { code: "uk", label: "UA 🇺🇦" },
 ];
+
+export const DEFAULT_LANGUAGE = LANGUAGES[0].code; // * "en"
+
+export const SUPPORTED_LANGUAGE_CODES = new Set(LANGUAGES.map(({ code }) => code));
