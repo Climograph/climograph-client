499bd657f0c62dd7172422d977c87216c37100d8
Andrew Kononenko <wastardy.k@gmail.com>
2026-04-07 17:21:51 +0200

    feat(ClimateStatistics): implement map click handling and enhance data fetching experience using custom hook

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,4 +2,5 @@ export { CellSizeSelector } from "./CellSizeSelector";
 export { LeafletMap } from "./LeafletMap";
 export { Navbar } from "./Navbar";
 export { SearchBar } from "./SearchBar";
+export { ThreeDotsScaleLoader } from "./svg";
 export { TemperatureChart } from "./TemperatureChart";
