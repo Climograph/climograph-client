c24f047093fd7a7dbce3fbbb05620ccf4bbe0c22
Andrew Kononenko <wastardy.k@gmail.com>
2026-04-15 22:32:33 +0200

    feat(constants): add CLIMATE_RANGE and CLIMATE_START to climate constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -7,7 +7,13 @@ export {
   WORLDCLIM_VARIABLE_BASE,
   WORLDCLIM_VARIABLES,
 } from "./api";
-export { DEFAULT_MAP_CENTER, MONTHS, MONTHS_ARRAY } from "./climate.constant";
+export {
+  CLIMATE_RANGE,
+  CLIMATE_START,
+  DEFAULT_MAP_CENTER,
+  MONTHS,
+  MONTHS_ARRAY,
+} from "./climate.constant";
 export { DEFAULT_CITY } from "./defaultCity.constant";
 export { ERROR_CONSTANTS } from "./error.constant";
 export { LOCAL_STORAGE_KEYS } from "./keys.constants";
