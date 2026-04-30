d5ad72d7bfffddcc3f2b1a48d12470fa7855fe0d
Andrew Kononenko <wastardy.k@gmail.com>
2026-04-30 14:51:53 +0200

    feat(types): simplify TMonthFilter type to support array of numbers or "all"

--- src/types/domain/climate.ts
+++ src/types/domain/climate.ts
@@ -3,20 +3,7 @@ import { CELL_SIZES, DATASETS, SIDEBAR_VARIABLES } from "@/constants";
 export type TCellSize = (typeof CELL_SIZES)[keyof typeof CELL_SIZES];
 export type TDataset = (typeof DATASETS)[keyof typeof DATASETS];
 export type TVariable = (typeof SIDEBAR_VARIABLES)[number];
-export type TMonthFilter =
-  | "all"
-  | "1"
-  | "2"
-  | "3"
-  | "4"
-  | "5"
-  | "6"
-  | "7"
-  | "8"
-  | "9"
-  | "10"
-  | "11"
-  | "12";
+export type TMonthFilter = number[] | "all";
 
 export type TMonthlyTemperature = {
   month: number;
