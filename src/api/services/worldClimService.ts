123be3a2ec3c964630f9e542d9d5870d6584c0a5
Andrew Kononenko <wastardy.k@gmail.com>
2026-05-01 13:57:53 +0200

    feat(service): update WorldClimService to enhance climate data retrieval for points

--- src/api/services/worldClimService.ts
+++ src/api/services/worldClimService.ts
@@ -1,11 +1,13 @@
+import type { TClimatePeriod } from "@/constants";
 import { WORLDCLIM_BASE_URL } from "@/constants";
 import type {
   TCellSize,
+  TVariable,
   TWorldClimAvgBoxResponse,
   TWorldClimBoxResponse,
   TWorldClimCellResponse,
   TWorldClimPixelResource,
-  TWorldClimPixelsResponse,
+  TWorldClimPointValueResponse,
 } from "@/types";
 import {
   buildDatasetParams,
@@ -27,26 +29,51 @@ export const WorldClimService = {
     return response.data;
   },
 
-  async getPixelsForPoint(
+  async getClimateDataForPoint(
     lat: number,
     lng: number,
     gridSize: TCellSize,
-    variables: string[],
-    isClimate: boolean,
-    year?: number,
-  ) {
-    const response = await axios.get<TWorldClimPixelsResponse>(`${WORLDCLIM_BASE_URL}/query`, {
+    variables: readonly TVariable[],
+    period: TClimatePeriod,
+  ): Promise<TWorldClimPointValueResponse> {
+    const response = await axios.get<TWorldClimPointValueResponse>(`${WORLDCLIM_BASE_URL}/query`, {
       params: {
-        id: "pixelsofapoint",
+        id: "pixelvaluesofapoint",
         lat,
         lng,
         grid: buildGridIri(gridSize),
         var: buildVariableIris(variables),
-        ...buildDatasetParams(isClimate, year),
+        isClimate: true,
       },
       headers: createWorldClimAuthHeaders(),
     });
+    validateResponseData(response);
+    return {
+      results: {
+        bindings: response.data.results.bindings.filter((b) => b.raster.value.includes(period)),
+      },
+    };
+  },
 
+  async getWeatherDataForPoint(
+    lat: number,
+    lng: number,
+    gridSize: TCellSize,
+    variables: readonly TVariable[],
+    year: number,
+  ): Promise<TWorldClimPointValueResponse> {
+    const response = await axios.get<TWorldClimPointValueResponse>(`${WORLDCLIM_BASE_URL}/query`, {
+      params: {
+        id: "pixelvaluesofapoint",
+        lat,
+        lng,
+        grid: buildGridIri(gridSize),
+        var: buildVariableIris(variables),
+        isWeather: true,
+        year,
+      },
+      headers: createWorldClimAuthHeaders(),
+    });
     validateResponseData(response);
     return response.data;
   },
